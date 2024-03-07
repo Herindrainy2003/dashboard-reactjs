880c5172520577f7fa331b68f1d0fe0caee38679
albert <herindrainyall2003@gmail.com>
2024-03-07 23:55:24 +0300

    first commit

---
+++ src/screens/OrderDetailScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Sidebar from "./../components/sidebar";
+import Header from "./../components/Header";
+import OrderDetailmain from "../components/orders/OrderDetailmain";
+import {useParams} from 'react-router-dom'
+const OrderDetailScreen = () => {
+  const orderId = useParams().id;
+  return (
+    <>
+      <Sidebar />
+      <main className="main-wrap">
+        <Header />
+        <OrderDetailmain orderId={orderId} />
+      </main>
+    </>
+  );
+};
+
+export default OrderDetailScreen;
